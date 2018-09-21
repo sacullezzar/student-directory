597a48fd13e8c9a3504f99c600036c229f691dca
sacullezzar <lucas.razzell@holidayextras.com>
2018-09-21 15:50:12 +0100

    interactive directory

--- directory.rb
+++ directory.rb
@@ -1,16 +1,15 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alec DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -25,6 +24,7 @@ def print_footer(names)
   print "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print_names_cohort(students)
 print_footer(students)
