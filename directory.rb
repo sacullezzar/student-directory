1f6ceb1f0c1fdd8bf0720e9e4fa4b77349af5651
sacullezzar <lucas.razzell@holidayextras.com>
2018-09-21 15:27:50 +0100

    further tidying

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
-student_count = 11
-
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alec DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Kreuger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"]
 puts "The students of Villains Academy"
 puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alec DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kreuger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-
-print "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+print "Overall, we have #{students.count} great students"
