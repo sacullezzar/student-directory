d83b6c4bb815b02433f119e61d5347d526d9b125
sacullezzar <lucas.razzell@holidayextras.com>
2018-09-24 13:58:15 +0100

    list by cohort

--- directory.rb
+++ directory.rb
@@ -1,43 +1,61 @@
+class Hash
+  def safe_invert
+    each_with_object({}) do |(key,value),out|
+      out[value] ||= []
+      out[value] << key
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-  puts "which cohort?"
-  cohort = gets.chomp
-  puts "what hobby?"
-  hobby = gets.chomp
-  puts "How tall?"
-  height = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november, hobby: hobby, height: height}
+    puts "which cohort?"
+    cohort = gets.chomp
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     name = gets.chomp
-end
+  end
   students
 end
 
 def print_header
   puts "The students of Villains Academy"
-  puts "--------------"
+  puts "-------------"
 end
 
-def print_names_cohort(names)
-  count = 0
-  unless names.length >= 4
-    names.each {|person| puts "#{person[:name]} (#{person[:cohort]} cohort)"}
-    end
-  until count == names.length/2
-  names.each {|person| puts "#{person[:name]} (#{person[:cohort]} cohort)"}
-  count += 2
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+
+
+def print_s_by_c(students)
+coh = students.map {|x| x.values}.to_h.safe_invert
+coh.each do |key,value|
+  puts "The students in the #{key} cohort are:"
+  value.each do |name|
+    puts name
+  end
 end
 end
 
 def print_footer(names)
-  print "Overall, we have #{names.count} great students"
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+end
 end
 
+#nothing happens until we call the methods
 students = input_students
 print_header
-print_names_cohort(students)
+#print(students)
+print_s_by_c(students)
 print_footer(students)
