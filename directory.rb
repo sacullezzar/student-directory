21442f7dbb9454db9e74e20b5a55fab229f50b5d
sacullezzar <lucas.razzell@holidayextras.com>
2018-09-24 16:57:32 +0100

    added split_to_hash

--- directory.rb
+++ directory.rb
@@ -1,10 +1,14 @@
 @students = []
 
+def split_to_hash(name, cohort = :november)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    split_to_hash(name, cohort)
   end
   file.close
 end
@@ -21,25 +25,20 @@ def try_load_students
   end
 end
 
-
-
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
 
-  # get the first name
   name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-  # add the student hash to the array
-  @students << {name: name, cohort: :november}
+
+  split_to_hash(name)
   puts "Now we have #{@students.count} students"
-  # get another name from the user
+
   name = STDIN.gets.chomp
   end
-  # return the array of students
+
 end
 
 def print_header
