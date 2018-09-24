16ff66edcc4306ba215b57f39db2e822eec3ccdd
sacullezzar <lucas.razzell@holidayextras.com>
2018-09-24 16:46:43 +0100

    loads .csv file

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 @students = []
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -9,20 +9,35 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
 
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
   @students << {name: name, cohort: :november}
   puts "Now we have #{@students.count} students"
   # get another name from the user
-  name = gets.chomp
+  name = STDIN.gets.chomp
   end
   # return the array of students
 end
@@ -52,7 +67,7 @@ end
 
 def show_students
   if @students.length == 0
-    print "No students entered"
+    puts "No students entered!"
   else
     print_header
     print_students(@students)
@@ -80,7 +95,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -94,4 +109,5 @@ def save_students
   file.close
 end
 
+try_load_students
 interactive_menu
